6c226c4747aea6f23bfaba841cbebeb5d97feb5e
kibet kilel <amos.rotich@strathmore.edu>
2022-03-03 08:55:27 +0300

    Add function

---
+++ Python/loops&lists/function.py
@@ -0,0 +1,6 @@
+def main():
+    print("I am part of the function.")
+    print("I am also part of the function.")pyt
+    print("Hey, me too!")
+    print("Sad not to be part of the function. I've been outdented.")
+    main()
